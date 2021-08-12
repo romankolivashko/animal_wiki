7fec0840a8642a816c528b0ce37fbf27f467aa0f
Roman Kolivashko <rkolivashko@gmail.com>
2021-08-12 10:50:20 -0700

    Business logic tested

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
-$(document).ready(function() {
-  $(".content").click(function() {
-    $(".card-showing").toggle();
-    $(".card-hidden").toggle();
-  });
-});
 
+$(document).ready(function() {
+  const animal = prompt("Lion, Zebra or Giraffe?");
+
+  if (animal === "lion") {
+    $('#lion_text').show();
+  } else if (animal === "zebra" ) {
+    $('#zebra_text').show();
+  } else if (animal === "giraffe") {
+    $('#giraffe_text').show();
+  }
+});
